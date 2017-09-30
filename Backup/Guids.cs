cdb0165a5a0106e0014ae3485f1d9c52d1d0062c
Nick Hynes <nhynes@outlook.com.au>
2017-09-30 22:34:37 +1000

    added backup content, please remove when verified

---
+++ Backup/Guids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pryda.GUnit
+{
+    static class GuidList
+    {
+        public const string guidGUnitPkgString = "ce7a7d8d-6c4b-4f9c-b77a-971914537c0b";
+        public const string guidGUnitCmdSetString = "ffadf28f-7099-46a1-a154-afc866e37545";
+        public const string guidToolWindowPersistanceString = "80a5c351-d0fa-4de6-99fc-9de062bc19b9";
+        public static readonly Guid guidGUnitCmdSet = new Guid(guidGUnitCmdSetString);
+    };
+}
---
+++ Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pryda Australia")]
+[assembly: AssemblyProduct("GUnit")]
+[assembly: AssemblyCopyright("Copyright © Pryda Australia 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.3.4.0")]
+[assembly: AssemblyFileVersion("1.3.4.0")]
+
+[assembly: InternalsVisibleTo("GUnit_IntegrationTestProject")]
+[assembly: InternalsVisibleTo("GUnit_UnitTestProject")]
